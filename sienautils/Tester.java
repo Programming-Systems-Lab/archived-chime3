b574ffbbc775e2a527f68cb6ec6c5b3a04319a90
daa82 <daa82>
2001-06-01 03:28:24 +0000

    Modified the Tester.java file to show Shen where his code fails...

--- sienautils/Tester.java
+++ sienautils/Tester.java
@@ -1,40 +1,50 @@
 import siena.*;
 
 public class Tester {
+
+	public static ThinClient siena;
+
+	public static void publishC_getRoom() {
+		Notification e = new Notification();
+
+		e.putAttribute("from_component", "client");
+		e.putAttribute("data", "http://www.google.com/");
+		e.putAttribute("auth", "true");
+		e.putAttribute("username", "denis");
+		e.putAttribute("password", "denis");
+		e.putAttribute("prot", "HTTP");
+		e.putAttribute("address", "http://www.google.com/");
+		e.putAttribute("access", "");
+		e.putAttribute("chime_method", "c_getRoom");
+
+		System.out.println("publishing " + e.toString());
+		try {
+			siena.publish(e);
+		} catch (SienaException ex) {
+			System.err.println("Siena error:" + ex.toString());
+		}
+   }
+
+
     public static void main(String[] args) {
-	try {
-	    if (args.length < 1) {
-		System.err.println("Usage:java Tester [server-uri]");
-		System.exit(1);
-	    }
-	    ThinClient siena;
-	    siena = new ThinClient(args[0]);
+		try {
+		    if (args.length < 1) {
+				System.err.println("Usage:java Tester [server-uri]");
+				System.exit(1);
+			}
 
-	    Notification e = new Notification();
+	    	siena = new ThinClient(args[0]);
 
-	    e.putAttribute("from_component", "client");
-	    e.putAttribute("data", "http://www.google.com/");
-	    e.putAttribute("auth", "true");
-	    e.putAttribute("username", "denis");
-	    e.putAttribute("password", "denis");
-	    e.putAttribute("prot", "HTTP");
-	    e.putAttribute("address", "http://www.google.com/");
-	    e.putAttribute("access", "");
-	    e.putAttribute("chime_method", "c_getRoom");
+	    	//this is the call
+	    	publishC_getRoom();
 
-	    System.out.println("publishing " + e.toString());
-	    try {
-		siena.publish(e);
-    } catch (SienaException ex) {
-	System.err.println("Siena error:" + ex.toString());
-    }
-	    System.out.println("shutting down.");
-	    siena.shutdown();
-	    //Thread.sleep(1000);
-	    //System.exit(0);
-	} catch (Exception ex) {
-	    ex.printStackTrace();
-	    System.exit(1);
+	    	System.out.println("shutting down.");
+	   		siena.shutdown();
+	    	//Thread.sleep(1000);
+	    	//System.exit(0);
+		} catch (Exception ex) {
+	    	ex.printStackTrace();
+	    	System.exit(1);
+		}
 	}
-    }
 }
