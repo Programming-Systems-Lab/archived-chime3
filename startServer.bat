a59322ad47cba47bf94e801235f9456813598920
daa82 <daa82>
2001-06-08 21:47:29 +0000

    Changed some batch files so Navdeep has an easier time running the server. The only environment variables that need to be changed are in setenv-vars.bat. Everything else gets them from this one file.

--- startServer.bat
+++ startServer.bat
@@ -1,14 +1,4 @@
-SET ROOT=C:\pslroot
-SET CHIME=%ROOT%\psl\chime
-SET FRAX=%CHIME%\frax
-SET JARS=%ROOT%\jars\siena-1.1.2.jar;%ROOT%\jars\HTTPClient.jar
-SET AUTH=%CHIME%\auth
-SET SIENAUTILS=%CHIME%\sienautils
-SET DATASERVER=%CHIME%\DataServer
-SET VEM=%CHIME%\vem
-SET EVENT_TRACER=%CHIME%\EventTracer
-SET SHEN=%ROOT%\jars\hsql.jar;%ROOT%\jars\jdom.jar;%ROOT%\jars\xerces.jar;%ROOT%\jars\collections.jar
-SET CLASSPATH=%ROOT%;%JARS%;%FRAX%;%AUTH%;%SIENAUTILS%;%SHEN%;%DATASERVER%;%EVENT_TRACER%;%VEM%;.
+call setenv-vars.bat
 
 rem del ET.*
 rem del DB.*
